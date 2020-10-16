3a3da0c7ed6a3f81c8945e30079dd6f35477858c
Selenia <isamara.lopez@gmail.com>
2020-10-16 09:14:56 -0600

    Login y Plantilla

---
+++ Areas/Identity/Data/E_RosarioUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_Rosario.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the E_RosarioUser class
+    public class E_RosarioUser : IdentityUser
+    {
+        [PersonalData]
+        [Column (TypeName ="varchar(100)")]
+        public string FirstName { get; set;  }
+
+        [PersonalData]
+        [Column(TypeName = "varchar(100)")]
+        public string LastName { get; set; }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using E_Rosario.Areas.Identity.Data;
+using E_Rosario.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(E_Rosario.Areas.Identity.IdentityHostingStartup))]
+namespace E_Rosario.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<E_RosarioDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("E_RosarioDbContextConnection")));
+
+                services.AddDefaultIdentity<E_RosarioUser>(options => { 
+                options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                 }
+
+
+                )
+                
+                    .AddEntityFrameworkStores<E_RosarioDbContext>();
+            });
+        }
+    }
+}
