b98789c962a7d076d2d383aef7633d4f5d040d07
Esteban David Ramirez Maradiaga <sparta117naruto@gmail.com>
2020-11-10 20:31:34 -0600

    creacion de roles

---
+++ Models/CreateRoleViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Rosario.Models
+{
+   
+        public class CreateRoleViewModel
+        {
+            public CreateRoleViewModel(string roleName)
+            {
+                RoleName = roleName;
+            }
+
+            [Required]
+            [Display(Name = "Role")]
+            public string RoleName { get; set; }
+        }
+}
